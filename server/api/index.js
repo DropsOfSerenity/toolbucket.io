5ca26a33b673db84ec026c6b39dbff9f8f97185e
Justin Schiff <AmbientRevolution@gmail.com>
2015-09-27 20:21:35 -0700

    add test coverage to api

--- server/api/index.js
+++ server/api/index.js
@@ -1,21 +1,136 @@
 var Hoek = require('hoek');
+var Joi = require('joi');
+var Boom = require('boom');
+var xml2js = require('xml2js');
+var req = require('request');
 
+var parser = new xml2js.Parser({
+    explicitArray: false
+});
+
+var USPS_VERIFICATION_API = 'http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=';
+var API_PARAM_MAP = {
+    Address2: 'address_line1',
+    Address1: 'address_line2',
+    City: 'address_city',
+    State: 'address_state',
+    Zip5: 'address_zip'
+};
+var USPS_API_USER_ID = '829ENIGM3242';
 
 exports.register = function (server, options, next) {
 
+    var validationSchema = Joi.alternatives().try(
+        Joi.object().keys({
+            address_line1: Joi.string().required(),
+            address_line2: Joi.string().allow('').default(''),
+            address_city: Joi.string().allow('').default(''),
+            address_state: Joi.string().allow('').default(''),
+            address_zip: Joi.string().required(),
+            address_country: Joi.string().allow('').default('').max(2)
+        }),
+        Joi.object().keys({
+            address_line1: Joi.string().required(),
+            address_line2: Joi.string().allow('').default(''),
+            address_city: Joi.string().required(),
+            address_state: Joi.string().required(),
+            address_zip: Joi.string().allow('').default(''),
+            address_country: Joi.string().allow('').default('').max(2)
+        })
+    );
+
     server.route({
-        method: 'GET',
-        path: '/',
+        method: 'POST',
+        path: '/verify/address',
         handler: function (request, reply) {
 
-            reply({ message: 'Welcome to the plot device.' });
-        }
-    });
+            var builder = new xml2js.Builder({
+                headless: true
+            });
+            var obj = {
+                AddressValidateRequest: {
+                    $: { USERID: USPS_API_USER_ID },
+                    Address: {
+                        Address1: request.payload.address_line2,
+                        Address2: request.payload.address_line1,
+                        City: request.payload.address_city,
+                        State: request.payload.address_state,
+                        Zip5: request.payload.address_zip,
+                        Zip4: ''
+                    }
+                }
+            };
+            var xml = builder.buildObject(obj);
+            var requestUrl = USPS_VERIFICATION_API + xml;
+            makeUSPSRequest(requestUrl, function (err, response) {
 
+                if (err) {
+                    reply(Boom.badData(err));
+                }
+                else {
+                    parseUSPSResponse(response, function (err, parsedResponse) {
+
+                        reply(parsedResponse);
+                    });
+                }
+            });
+        },
+
+        config: {
+            validate: {
+                payload: validationSchema,
+                failAction: function (request, reply, source, error) {
+
+                    reply(Boom.badData(error.data));
+                }
+            }
+        }
+
+    });
 
     next();
 };
 
+var makeUSPSRequest = function (requestUrl, cb) {
+
+    req.get({
+        url: requestUrl,
+        headers: { 'Content-Type': 'text/xml' }
+    }, function (error, response, body) {
+
+        parser.parseString(body, function (err, result) {
+
+            var resError = result.AddressValidateResponse.Address.Error;
+            if (resError) {
+                return cb(resError.Description.trim());
+            }
+            cb(null, result.AddressValidateResponse.Address);
+        });
+    });
+};
+
+var parseUSPSResponse = function (resp, cb) {
+
+    var parsedResponse = {};
+    for (var field in resp) {
+        var convertedField = API_PARAM_MAP[field];
+        if (convertedField) {
+            parsedResponse[convertedField] = resp[field];
+            if (convertedField === 'address_zip' && resp.Zip4) {
+                parsedResponse[convertedField] += '-' + resp.Zip4;
+            }
+        }
+    }
+
+    if (resp.ReturnText) {
+        var message = resp.ReturnText;
+    }
+
+    return cb(null, {
+        message: message,
+        address: parsedResponse
+    });
+};
 
 exports.register.attributes = {
     name: 'api'
--- test/server/api/index.js
+++ test/server/api/index.js
@@ -3,10 +3,12 @@ var Code = require('code');
 var Config = require('../../../config');
 var Hapi = require('hapi');
 var IndexPlugin = require('../../../server/api/index');
+var Sinon = require('sinon');
+var request = require('request');
 
 
 var lab = exports.lab = Lab.script();
-var request, server;
+var invalidUSPSResponse, requestStub, server, validRequest, validUSPSResponse;
 
 
 lab.beforeEach(function (done) {
@@ -27,23 +29,60 @@ lab.beforeEach(function (done) {
 
 lab.experiment('Index Plugin', function () {
 
+    validUSPSResponse = '<?xml version="1.0" encoding="UTF-8"?><AddressValidateResponse><Address><Address2>1600 AMPHITHEATRE PKWY</Address2><City>MOUNTAIN VIEW</City><State>CA</State><Zip5>94043</Zip5><Zip4>1351</Zip4></Address></AddressValidateResponse>';
+    invalidUSPSResponse = '<?xml version="1.0" encoding="UTF-8"?><AddressValidateResponse><Address><Error><Number>-2147219401</Number><Source>clsAMS</Source><Description>Address Not Found.  </Description><HelpFile/><HelpContext/></Error></Address></AddressValidateResponse>';
+    validRequest = {
+        method: 'POST',
+        url: '/verify/address',
+        payload: {
+            address_line1: '1600 Amphitheatre Pkwy',
+            address_zip: '94043'
+        }
+    };
+
     lab.beforeEach(function (done) {
 
-        request = {
-            method: 'GET',
-            url: '/'
-        };
-
+        requestStub = Sinon.stub(request, 'get');
         done();
     });
 
+    lab.afterEach(function (done) {
 
-    lab.test('it returns the default message', function (done) {
+        requestStub.restore();
+        done();
+    });
 
-        server.inject(request, function (response) {
+    lab.test('it works with a valid request', function (done) {
 
-            Code.expect(response.result.message).to.match(/welcome to the plot device/i);
+        requestStub.yields(null, { statusCode: 200 }, validUSPSResponse);
+        server.inject(validRequest, function (response) {
+
             Code.expect(response.statusCode).to.equal(200);
+
+            done();
+        });
+    });
+
+    lab.test('it handles an error response from USPS', function (done) {
+
+        requestStub.yields(null, { statusCode: 200 }, invalidUSPSResponse);
+        server.inject(validRequest, function (response) {
+
+            Code.expect(response.statusCode).to.equal(422);
+
+            done();
+        });
+    });
+
+    lab.test('it returns 400 with an invalid request', function (done) {
+
+        var invalidRequest = {
+            method: 'POST',
+            url: '/verify/address'
+        };
+        server.inject(invalidRequest, function (response) {
+
+            Code.expect(response.statusCode).to.equal(422);
 
             done();
         });
